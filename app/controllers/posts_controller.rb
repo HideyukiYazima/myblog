8f6264a06e8fdd459b463daa66429690ea0a0a45
Yazima <yazima04200@gmail.com>
2022-07-15 20:11:24 +0900

    Editと削除追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: 'desc')
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
@@ -29,8 +29,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :body)
     end
+
 end
