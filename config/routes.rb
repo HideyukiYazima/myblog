1fb40f64190941cb0d86880c7fc385c845706edd
Yazima <yazima04200@gmail.com>
2022-10-27 21:22:17 +0900

    ユーザー登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
   get "users/index" => "users#index"
+  get "users/:id" => "users#show"
 
 end
